9d5ea0052057014cf8174a7b88dacfed1ad0abcd
Galadima3 <abrahamgaladima300@gmail.com>
2022-08-26 15:43:47 +0100

    Splash Screen Added

---
+++ lib/Screens/welcome_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TestX extends StatelessWidget {
+  const TestX({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+    );
+  }
+}
